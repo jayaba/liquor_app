4ff45995438ebabf6312fde48e5abde412476a1f
jayaba <erangajayaba0@gmail.com>
2023-03-03 00:19:23 +0530

    homepage added

--- lib/App_Colors.dart
+++ lib/App_Colors.dart
@@ -3,4 +3,5 @@ import 'package:flutter/material.dart';
 class AppColors {
   final main_color = const Color(0xFF46DE99);
   final onboard_screen = const Color(0xFFF6FFF6);
+  final tile_text_color = const Color(0xFF979797);
 }
--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -31,44 +31,52 @@ class _HomeScreenState extends State<HomeScreen> {
           ),
         ),
       ),
-      body: Column(children: [
-        NowTrendingTab(),
-        RowCtExplore(),
-        Row(
-          children: [
-            Container(
-              margin: EdgeInsets.only(left: 5.w, top: 2.h),
-              height: 19.h,
-              width: 30.w,
-              decoration: BoxDecoration(
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.grey.withOpacity(0.2),
-                    // spreadRadius: 2,
-                    // blurRadius: 50,
-                    offset: Offset(0, 0), // changes position of shadow
+      body: Stack(children: [
+        Positioned(child: NowTrendingTab()),
+        Positioned(top: 20.h, child: RowCtExplore()),
+        ListView(scrollDirection: Axis.horizontal, children: [
+          Row(
+            children: [
+              CategoryBeer(appColors: appColors),
+              CategoryDCSL(appColors: appColors),
+              CategoryMENDIS(appColors: appColors),
+              CategoryROCKLAND(appColors: appColors),
+              CategoryIDL(appColors: appColors),
+            ],
+          ),
+        ]),
+        Positioned(
+          top: 51.h,
+          child: Container(
+              margin: EdgeInsets.only(
+                  bottom: 32.h, top: 1.h, right: 26.h, left: 2.h),
+              // height: 2.h,
+              // width: 10.h,
+              child: Text(
+                "Popular Liquor",
+                style: GoogleFonts.poppins(
+                  textStyle: TextStyle(
+                    color: Colors.black,
+                    fontSize: 20,
+                    fontWeight: FontWeight.w500,
                   ),
-                ],
-                border: Border.all(
-                  color: Color.fromARGB(255, 145, 145, 145),
                 ),
-                borderRadius: BorderRadius.circular(20),
-              ),
-              child: InkWell(
-                customBorder: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(20),
-                ),
-                overlayColor: MaterialStateProperty.all(appColors.main_color),
-                onTap: () => print("tapped"),
-                radius: 60,
-                child: Ink.image(
-                    fit: BoxFit.contain, // Fixes border issues
-                    child:
-                        Container(width: 2.h, height: 2.h, color: Colors.red),
-                    image: AssetImage("assets/appbar_icon.png")),
-              ),
+              )),
+        ),
+        Positioned(
+          top: 56.h,
+          left: 2.5.h,
+          child: Container(
+            color: Colors.black,
+            height: 40.h,
+            width: 90.w,
+            child: ListView(
+              children: [
+                tileOne(appColors: appColors),
+                tileOne(appColors: appColors),
+              ],
             ),
-          ],
+          ),
         )
       ]),
     );
@@ -76,7 +84,6 @@ class _HomeScreenState extends State<HomeScreen> {
 
   Row RowCtExplore() {
     return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
         Container(
           margin: EdgeInsets.only(top: 4.h, left: 2.h),
@@ -91,8 +98,13 @@ class _HomeScreenState extends State<HomeScreen> {
             ),
           ),
         ),
+        // SizedBox(
+        //   width: 13.h,
+
+        // ),
         Container(
-          margin: EdgeInsets.only(top: 4.h, right: 2.h),
+          // alignment: Alignment.centerRight,
+          margin: EdgeInsets.fromLTRB(12.h, 4.h, 0.h, 0),
           child: TextButton(
             onPressed: () {
               print("show all");
@@ -182,6 +194,331 @@ class _HomeScreenState extends State<HomeScreen> {
           ),
         )
       ],
+    );
+  }
+}
+
+class tileOne extends StatelessWidget {
+  const tileOne({
+    super.key,
+    required this.appColors,
+  });
+
+  final AppColors appColors;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(top: 2.h),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20),
+        color: Colors.white,
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.5),
+            spreadRadius: 5,
+            blurRadius: 7,
+            offset: Offset(0, 3), // changes position of shadow
+          ),
+        ],
+      ),
+      height: 10.h,
+      width: 90.w,
+      child: ListTile(
+        leading: Container(
+          margin: EdgeInsets.only(top: 1.h),
+          child: Image.asset(
+            "assets/carsberg1.png",
+          ),
+        ),
+        title: Container(
+          margin: EdgeInsets.only(top: 2.h),
+          child: Text(
+            "Carlsberg",
+            style: GoogleFonts.poppins(
+              textStyle: TextStyle(
+                color: Colors.black,
+                fontSize: 20,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+          ),
+        ),
+        subtitle: Container(
+          child: Text(
+            "500 ml | 4.4%",
+            style: GoogleFonts.poppins(
+              textStyle: TextStyle(
+                color: appColors.tile_text_color,
+                fontSize: 13,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+          ),
+        ),
+        isThreeLine: true,
+        trailing: Container(
+          margin: EdgeInsets.only(top: 2.5.h),
+          child: Text(
+            "Rs. 650",
+            style: GoogleFonts.poppins(
+              textStyle: TextStyle(
+                color: appColors.main_color,
+                fontSize: 20,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+          ),
+        ),
+      ),
+      // child: ListTile(
+
+      // ),
+    );
+  }
+}
+
+class CategoryIDL extends StatelessWidget {
+  const CategoryIDL({
+    super.key,
+    required this.appColors,
+  });
+
+  final AppColors appColors;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(left: 5.w, top: 0.h, bottom: 8.h),
+      height: 19.h,
+      width: 30.w,
+      decoration: BoxDecoration(
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.2),
+            // spreadRadius: 2,
+            // blurRadius: 50,
+            offset: Offset(0.h, 0.h), // changes position of shadow
+          ),
+        ],
+        border: Border.all(
+          color: Color.fromARGB(255, 145, 145, 145),
+        ),
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: InkWell(
+        customBorder: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20),
+        ),
+        overlayColor: MaterialStateProperty.all(appColors.main_color),
+        onTap: () => print("tapped"),
+        radius: 60,
+        child: Ink.image(
+            fit: BoxFit.contain, // Fixes border issues
+            child: Container(
+                alignment: Alignment.bottomCenter,
+                child: Text("I.D.L",
+                    style:
+                        TextStyle(fontSize: 16, fontWeight: FontWeight.bold))),
+            image: AssetImage("assets/idl_logo.png"),
+            alignment: Alignment.center),
+      ),
+    );
+  }
+}
+
+class CategoryROCKLAND extends StatelessWidget {
+  const CategoryROCKLAND({
+    super.key,
+    required this.appColors,
+  });
+
+  final AppColors appColors;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(left: 5.w, top: 0.h, bottom: 8.h),
+      height: 19.h,
+      width: 30.w,
+      decoration: BoxDecoration(
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.2),
+            // spreadRadius: 2,
+            // blurRadius: 50,
+            offset: Offset(0.h, 0.h), // changes position of shadow
+          ),
+        ],
+        border: Border.all(
+          color: Color.fromARGB(255, 145, 145, 145),
+        ),
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: InkWell(
+        customBorder: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20),
+        ),
+        overlayColor: MaterialStateProperty.all(appColors.main_color),
+        onTap: () => print("tapped"),
+        radius: 60,
+        child: Ink.image(
+            fit: BoxFit.contain, // Fixes border issues
+            child: Container(
+                alignment: Alignment.bottomCenter,
+                child: Text("Rockland",
+                    style:
+                        TextStyle(fontSize: 16, fontWeight: FontWeight.bold))),
+            image: AssetImage("assets/rockland_logo.png"),
+            alignment: Alignment.center),
+      ),
+    );
+  }
+}
+
+class CategoryMENDIS extends StatelessWidget {
+  const CategoryMENDIS({
+    super.key,
+    required this.appColors,
+  });
+
+  final AppColors appColors;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(left: 5.w, top: 0.h, bottom: 8.h),
+      height: 19.h,
+      width: 30.w,
+      decoration: BoxDecoration(
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.2),
+            // spreadRadius: 2,
+            // blurRadius: 50,
+            offset: Offset(0.h, 0.h), // changes position of shadow
+          ),
+        ],
+        border: Border.all(
+          color: Color.fromARGB(255, 145, 145, 145),
+        ),
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: InkWell(
+        customBorder: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20),
+        ),
+        overlayColor: MaterialStateProperty.all(appColors.main_color),
+        onTap: () => print("tapped"),
+        radius: 60,
+        child: Ink.image(
+            fit: BoxFit.contain, // Fixes border issues
+            child: Container(
+                alignment: Alignment.bottomCenter,
+                child: Text("Mendis",
+                    style:
+                        TextStyle(fontSize: 16, fontWeight: FontWeight.bold))),
+            image: AssetImage("assets/mendis_logo.png"),
+            alignment: Alignment.center),
+      ),
+    );
+  }
+}
+
+class CategoryDCSL extends StatelessWidget {
+  const CategoryDCSL({
+    super.key,
+    required this.appColors,
+  });
+
+  final AppColors appColors;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(left: 5.w, top: 0.h, bottom: 8.h),
+      height: 19.h,
+      width: 30.w,
+      decoration: BoxDecoration(
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.2),
+            // spreadRadius: 2,
+            // blurRadius: 50,
+            offset: Offset(0.h, 0.h), // changes position of shadow
+          ),
+        ],
+        border: Border.all(
+          color: Color.fromARGB(255, 145, 145, 145),
+        ),
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: InkWell(
+        customBorder: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20),
+        ),
+        overlayColor: MaterialStateProperty.all(appColors.main_color),
+        onTap: () => print("tapped"),
+        radius: 60,
+        child: Ink.image(
+            fit: BoxFit.contain, // Fixes border issues
+            child: Container(
+                alignment: Alignment.bottomCenter,
+                child: Text("D.C.S.L",
+                    style:
+                        TextStyle(fontSize: 16, fontWeight: FontWeight.bold))),
+            image: AssetImage("assets/dcsl_logo.png"),
+            alignment: Alignment.center),
+      ),
+    );
+  }
+}
+
+class CategoryBeer extends StatelessWidget {
+  const CategoryBeer({
+    super.key,
+    required this.appColors,
+  });
+
+  final AppColors appColors;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(left: 5.w, top: 0.h, bottom: 8.h),
+      height: 19.h,
+      width: 30.w,
+      decoration: BoxDecoration(
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.2),
+            // spreadRadius: 2,
+            // blurRadius: 50,
+            offset: Offset(0, 0), // changes position of shadow
+          ),
+        ],
+        border: Border.all(
+          color: Color.fromARGB(255, 145, 145, 145),
+        ),
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: InkWell(
+        customBorder: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20),
+        ),
+        overlayColor: MaterialStateProperty.all(appColors.main_color),
+        onTap: () => print("tapped"),
+        radius: 60,
+        child: Ink.image(
+            fit: BoxFit.contain, // Fixes border issues
+            child: Container(
+                alignment: Alignment.bottomCenter,
+                child: Text("Beer",
+                    style:
+                        TextStyle(fontSize: 16, fontWeight: FontWeight.bold))),
+            image: AssetImage("assets/beer_logo.png"),
+            alignment: Alignment.center),
+      ),
     );
   }
 }
