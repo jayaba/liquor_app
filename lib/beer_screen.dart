fa845d956d445844d3f1289f729545bcb35f1834
jayaba <erangajayaba0@gmail.com>
2023-03-04 23:47:58 +0530

    add other beer cate

--- lib/beer_screen.dart
+++ lib/beer_screen.dart
@@ -33,57 +33,22 @@ class BeerScreen extends StatelessWidget {
         // 1st Row
         LionLarger(appColors: appColors),
         Container(
+          /////Right side///////////////////////////////////////////////
 
-
-          /////Right side 
-          //color: Color.fromARGB(255, 217, 218, 219),
-          margin: EdgeInsets.only(top: 2.h), // whole container
+          margin: EdgeInsets.only(top: 4.h), // whole container
           height: 25.h,
           width: 90.w,
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: [
-              Stack(
-                children: [
-                  Container(
-                    margin: EdgeInsets.only(left: 1.h, top: 2.5.h),
-                    height: 22.h, //frame circle
-                    width: 22.h,
-                    decoration: BoxDecoration(
-                      // color: Color.fromARGB(255, 0, 170, 162),
-                      borderRadius: BorderRadius.circular(150),
-                      border: Border.all(
-                        color: appColors.main_color,
-                        width: 1,
-                      ),
-                    ),
-                  ),
-                  Container(
-                    margin: EdgeInsets.only(left: 2.h, top: 3.5.h),
-                    height: 20.h,
-                    width: 20.h,
-                    decoration: BoxDecoration(
-                      color: appColors.circle_background, //inside circle
-                      borderRadius: BorderRadius.circular(150),
-                    ),
-                  ),
-                  Container(
-                    margin: EdgeInsets.only(left: 2.6.h, top: 4.3.h),
-                    height: 18.h,
-                    width: 18.h,
-                    // color: Colors.amberAccent,
-                    child: Image.asset("assets/LION-LAGER.png"), //image cont
-                  ),
-                ],
-              ),
               Column(
                 children: [
                   Container(
-                    margin: EdgeInsets.only(top: 2.h, left: 1.h),
+                    margin: EdgeInsets.only(top: 3.h, left: 2.h),
                     width: 14.5.h,
-                    height: 4.h,
+                    height: 3.h,
                     child: Text(
-                      "Lion Lager",
+                      "Lion Stout",
                       style: GoogleFonts.philosopher(
                         //title
                         //Title
@@ -97,24 +62,21 @@ class BeerScreen extends StatelessWidget {
                     ),
                   ),
                   Container(
-                    margin: EdgeInsets.only(left: 2.h),
+                    margin: EdgeInsets.only(left: 3.h, top: 2.h),
                     width: 15.h,
-                    height: 13.h,
+                    height: 11.h,
                     child: Text(
-                        "Undoubtedly, \nThe best-selling among our mild beers,\nLion Lager has a 4.8% alcohol volume and is credited as a great thirst quencher.",
+                        "This amber golden beer adds a definite kick to the unique flavour that's a signature of the Lion Family of Beers. With an alcohol content of 8.8%.",
                         style: GoogleFonts.poppins(
                           textStyle: TextStyle(
                             color: Colors.black,
-                            fontSize: 1.6.h,
+                            fontSize: 1.4.h,
                             fontWeight: FontWeight.normal,
                           ),
                         )),
                   ),
-                  SizedBox(
-                    height: 1.h,
-                  ),
                   Container(
-                      margin: EdgeInsets.only(left: 1.h),
+                      margin: EdgeInsets.only(left: 3.h, bottom: 0.h),
                       height: 5.h,
                       width: 15.h,
                       child: OutlinedButton(
@@ -156,7 +118,121 @@ class BeerScreen extends StatelessWidget {
                         ),
                       ))
                 ],
-              )
+              ),
+              Stack(
+                children: [
+                  Container(
+                    margin: EdgeInsets.only(left: 0.h, top: 2.5.h),
+                    height: 22.h, //frame circle
+                    width: 22.h,
+                    decoration: BoxDecoration(
+                      // color: Color.fromARGB(255, 0, 170, 162),
+                      borderRadius: BorderRadius.circular(150),
+                      border: Border.all(
+                        color: appColors.main_color,
+                        width: 1,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(left: 1.h, top: 3.5.h),
+                    height: 20.h,
+                    width: 20.h,
+                    decoration: BoxDecoration(
+                      color: appColors.circle_background, //inside circle
+                      borderRadius: BorderRadius.circular(150),
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(left: 2.1.h, top: 4.3.h),
+                    height: 18.h,
+                    width: 18.h,
+                    // color: Colors.amberAccent,
+                    child: Image.asset("assets/strong.png"), //image cont
+                  ),
+                ],
+              ),
+              // Column(
+              //   children: [
+              //     Container(
+              //       margin: EdgeInsets.only(top: 2.h, left: 1.h),
+              //       width: 14.5.h,
+              //       height: 4.h,
+              //       child: Text(
+              //         "Lion Lager",
+              //         style: GoogleFonts.philosopher(
+              //           //title
+              //           //Title
+              //           letterSpacing: 1,
+              //           textStyle: TextStyle(
+              //             color: Colors.black,
+              //             fontSize: 3.h,
+              //             fontWeight: FontWeight.w600,
+              //           ),
+              //         ),
+              //       ),
+              //     ),
+              //     Container(
+              //       margin: EdgeInsets.only(left: 2.h),
+              //       width: 15.h,
+              //       height: 13.h,
+              //       child: Text(
+              //           "Undoubtedly, \nThe best-selling among our mild beers,\nLion Lager has a 4.8% alcohol volume and is credited as a great thirst quencher.",
+              //           style: GoogleFonts.poppins(
+              //             textStyle: TextStyle(
+              //               color: Colors.black,
+              //               fontSize: 1.6.h,
+              //               fontWeight: FontWeight.normal,
+              //             ),
+              //           )),
+              //     ),
+              //     SizedBox(
+              //       height: 1.h,
+              //     ),
+              //     Container(
+              //         margin: EdgeInsets.only(left: 1.h),
+              //         height: 5.h,
+              //         width: 15.h,
+              //         child: OutlinedButton(
+              //           //button start
+              //           style: OutlinedButton.styleFrom(
+              //             side: BorderSide(
+              //               width: 0.2.h,
+              //               color: appColors.main_color,
+              //               style: BorderStyle.solid,
+              //             ),
+              //           ),
+              //           onPressed: () {
+              //             print("sheck");
+              //           },
+              //           child: Row(
+              //             mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              //             children: [
+              //               Container(
+              //                 margin: EdgeInsets.only(left: 0.6.h),
+              //                 child: Text(
+              //                   "Check It Out",
+              //                   style: GoogleFonts.poppins(
+              //                     textStyle: TextStyle(
+              //                       color: Colors.black,
+              //                       fontSize: 1.6.h,
+              //                       fontWeight: FontWeight.w500,
+              //                     ),
+              //                   ),
+              //                 ),
+              //               ),
+              //               Container(
+              //                 child: Icon(
+              //                   Icons.arrow_forward_ios, //button end
+              //                   color: Colors.black,
+              //                   size: 1.h,
+              //                 ),
+              //               ),
+              //             ],
+              //           ),
+              //         ))
+              //   ],
+              // )
             ],
           ),
         ),
