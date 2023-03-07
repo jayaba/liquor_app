a86a5baa935d74a3199cdd6327da4a985a77e8de
jayaba <erangajayaba0@gmail.com>
2023-03-07 15:25:51 +0530

    alignment issue resolve

--- lib/lion_larger.dart
+++ lib/lion_larger.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:liquor_app_sinhala/App_Colors.dart';
 import 'package:sizer/sizer.dart';
 
 class LionLargerScreen extends StatelessWidget {
@@ -7,6 +8,7 @@ class LionLargerScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    AppColors appColors = AppColors();
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
@@ -32,24 +34,143 @@ class LionLargerScreen extends StatelessWidget {
             height: 2.h,
           ),
           Container(
-            height: 10.h,
+            height: 6.3.h,
             width: 80.w,
             child: Text(
               'Lion Lager',
               style: GoogleFonts.poppins(
                 textStyle: TextStyle(
                   color: Colors.black,
-                  fontSize: 6.h,
+                  fontSize: 5.8.h,
                   fontWeight: FontWeight.w500,
                 ),
               ),
             ),
           ),
-
-          Row(
-            children: [
-              
-            ],
+          Container(
+            margin: EdgeInsets.only(right: 41.w),
+            height: 4.2.h,
+            width: 38.w,
+            child: Text(
+              '500ml Can  |  8.8%\n625ml Bottle  |  8.8%',
+              style: GoogleFonts.poppins(
+                textStyle: TextStyle(
+                  color: Colors.grey,
+                  fontSize: 1.7.h,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 2.h,
+          ),
+          Container(
+            margin: EdgeInsets.only(right: 29.w),
+            height: 3.2.h,
+            width: 50.w,
+            child: Text(
+              'Price',
+              style: GoogleFonts.poppins(
+                textStyle: TextStyle(
+                  color: appColors.main_color,
+                  fontSize: 3.5.h,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 1.h,
+          ),
+          Container(
+            width: 85.w,
+            margin: EdgeInsets.only(right: 6.5.h),
+            child: Row(
+              // mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Container(
+                  margin: EdgeInsets.only(left: 4.8.h),
+                  height: 3.4.h,
+                  width: 35.w,
+                  child: Text(
+                    'LKR 410.00',
+                    style: GoogleFonts.sono(
+                      textStyle: TextStyle(
+                        color: Colors.black,
+                        fontSize: 3.h,
+                        fontWeight: FontWeight.w400,
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  //     color: Colors.amberAccent[200],
+                  height: 2.h,
+                  width: 38.w,
+                  child: Text(
+                    '|  500ml Can',
+                    style: GoogleFonts.poppins(
+                      textStyle: TextStyle(
+                        color: Colors.grey,
+                        fontSize: 1.7.h,
+                        fontWeight: FontWeight.w400,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            width: 85.w,
+            margin: EdgeInsets.only(right: 6.5.h),
+            child: Row(
+              children: [
+                Container(
+                  margin: EdgeInsets.only(left: 4.8.h),
+                  height: 3.4.h,
+                  width: 35.w,
+                  child: Text(
+                    'LKR 410.00',
+                    style: GoogleFonts.sono(
+                      textStyle: TextStyle(
+                        color: Colors.black,
+                        fontSize: 3.h,
+                        fontWeight: FontWeight.w400,
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  //     color: Colors.amberAccent[200],
+                  height: 2.h,
+                  width: 38.w,
+                  child: Text(
+                    '|  625ml Bottle',
+                    style: GoogleFonts.poppins(
+                      textStyle: TextStyle(
+                        color: Colors.grey,
+                        fontSize: 1.7.h,
+                        fontWeight: FontWeight.w400,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(right: 29.w),
+            height: 4.h,
+            width: 50.w,
+            child: Text(
+              'Tasting Note',
+              style: GoogleFonts.poppins(
+                  fontSize: 3.5.h,
+                  color: appColors.main_color,
+                  fontWeight: FontWeight.w500),
+            ),
           )
         ],
       ),
